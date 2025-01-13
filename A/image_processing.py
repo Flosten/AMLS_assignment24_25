f7a28c81daa5b0a775899fa3876d89236b68ab7e
Yikun Li <uceeyl8@ucl.ac.uk>
2025-01-13 22:33:00 +0000

    fix(task A): complete model selection and supplementation

--- A/image_processing.py
+++ A/image_processing.py
@@ -1,15 +1,18 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import pywt
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from sklearn import svm
+from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score
+from sklearn.metrics.pairwise import linear_kernel, rbf_kernel
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 
-from A.data_acquiring import *
-from A.visualising import *
+from A.visualising import model_evaluation, train_val_loss_plot
 
 
 def image_preprocess(images: np.ndarray, labels: np.ndarray):
@@ -29,12 +32,89 @@ def image_preprocess(images: np.ndarray, labels: np.ndarray):
     return images_dim_1, labels_dim_1
 
 
+# def wavelet_transform(images: np.ndarray, labels: np.ndarray):
+#     """
+#     Perform wavelet transform on the images.
+
+#     Args:
+#         images (np.ndarray): Input images set.
+#         wavelet (str): The wavelet type to be used.
+
+#     Returns:
+#         tuple: Image and label set after feature extraction.
+#     """
+#     # get the images wavelet features and labels
+#     images_wavelet = []
+#     for image in images:
+#         coeffs = pywt.dwt2(image, "haar")
+#         ll, (lh, hl, hh) = coeffs
+#         images_wavelet.append(
+#             # np.concatenate([ll.flatten(), lh.flatten(), hl.flatten(), hh.flatten()])
+#             np.concatenate([lh.flatten(), hl.flatten(), hh.flatten()])
+#         )
+
+#     images_wavelet = np.array(images_wavelet)
+#     labels_wavelet = labels.flatten()
+
+#     return images_wavelet, labels_wavelet
+
+
+def PCA_preprocess(train_images: np.ndarray, rate: float):
+
+    pca = PCA(rate)
+    train_images_pca = pca.fit_transform(train_images)
+    # print(f"PCA: {train_images_pca.shape[1]}")
+
+    return train_images_pca, pca
+
+
 def svm_train(
     images_train: np.ndarray,
     labels_train: np.ndarray,
     images_val: np.ndarray,
     labels_val: np.ndarray,
     kernal: str,
+    regularization: float,
+):
+
+    # standardize the data
+    scaler = StandardScaler()
+    images_train_std = scaler.fit_transform(images_train)
+
+    # train the model  C=0.005 0.002
+    svm_model = svm.SVC(
+        kernel=kernal,
+        C=regularization,
+        class_weight="balanced",
+        tol=0.001,
+        random_state=711,
+    )
+    svm_model.fit(images_train_std, labels_train)
+
+    # evaluate the model
+    images_val_std = scaler.transform(images_val)
+    label_pred = svm_model.predict(images_val_std)
+    accuracy = accuracy_score(labels_val, label_pred)
+
+    # print the accuracy on the training set
+    train_label_pred = svm_model.predict(images_train_std)
+    train_accuracy = accuracy_score(labels_train, train_label_pred)
+    print(f"the SVM's accuracy on the training set is: {train_accuracy}")
+
+    # print the accuracy on the validation set
+    print(f"the SVM's accuracy on the validation set is: {accuracy}")
+
+    return svm_model, scaler
+
+
+def svm_train_cross_val(
+    images_train: np.ndarray,
+    labels_train: np.ndarray,
+    images_val: np.ndarray,
+    labels_val: np.ndarray,
+    kernal: str,
+    regularization: float,
+    other_params: float,
 ):
     """
     Train an SVM model and return the trained model along with the scaler.
@@ -51,92 +131,93 @@ def svm_train(
             - sklearn.preprocessing.StandardScaler:
                 The StandardScaler used to standardize the training data.
     """
+    # concatenate the training and validation set
+    images_train = np.concatenate((images_train, images_val), axis=0)
+    labels_train = np.concatenate((labels_train, labels_val), axis=0)
+
     # standardize the data
     scaler = StandardScaler()
     images_train_std = scaler.fit_transform(images_train)
 
-    # train the model
-    svm_model = svm.SVC(kernel=kernal, random_state=711)
+    svm_model = None
+    # train the model  C=0.005
+    if kernal == "linear":
+        svm_model = svm.SVC(
+            kernel=kernal,
+            C=regularization,
+            class_weight="balanced",
+            tol=0.001,
+            random_state=711,
+        )
+    elif kernal == "rbf":
+        svm_model = svm.SVC(
+            kernel=kernal,
+            C=regularization,
+            class_weight="balanced",
+            tol=0.001,
+            random_state=711,
+            gamma=other_params,
+        )
+    elif kernal == "poly":
+        svm_model = svm.SVC(
+            kernel=kernal,
+            C=regularization,
+            class_weight="balanced",
+            tol=0.001,
+            random_state=711,
+            degree=other_params,
+        )
     svm_model.fit(images_train_std, labels_train)
 
-    # evaluate the model
-    images_val_std = scaler.transform(images_val)
-    label_pred = svm_model.predict(images_val_std)
-    accuracy = accuracy_score(labels_val, label_pred)
+    # print the accuracy on the training set
+    train_label_pred = svm_model.predict(images_train_std)
+    train_accuracy = accuracy_score(labels_train, train_label_pred)
+    print(f"the SVM's accuracy on the training set is: {train_accuracy}")
 
-    # print the accuracy
-    print(
-        f"Without hyperparameter selection, the SVM's accuracy on the validation set is: {accuracy}"
+    # cross validation
+    scores = cross_val_score(
+        svm_model, images_train_std, labels_train, cv=5, scoring="accuracy"
     )
+    print(f"Cross validation scores: {scores}")
+    print(f"Mean cross validation score: {scores.mean()}")
 
     return svm_model, scaler
 
 
-def svm_train_best_params(
-    images_train: np.ndarray,
-    images_val: np.ndarray,
-    labels_train: np.ndarray,
-    labels_val: np.ndarray,
-    kernal: str,
+def svm_test_cross_val(
+    images_test: np.ndarray,
+    labels_test: np.ndarray,
+    model: svm.SVC,
+    scaler: StandardScaler,
 ):
     """
-    Find the best hyperparameters for the SVM model.
+    Test the SVM model.
 
     Args:
-        images_train (np.ndarray): Input images training set.
-        images_val (np.ndarray): Input images validation set.
-        labels_train (np.ndarray): Corresponding labels for the training set.
-        labels_val (np.ndarray): Corresponding labels for the validation set.
-        kernal (str): The kernel type to be used in the SVM model.
+        images (np.ndarray): Input images test set.
+        labels (np.ndarray): Corresponding labels for the test set.
+        model (svm.SVC): The trained SVM model.
+        scaler (StandardScaler): The StandardScaler used to standardize the data.
 
     Returns:
         tuple:
-            - sklearn.svm.SVC:
-                The best trained Support Vector Classifier (SVC) object.
-            - sklearn.preprocessing.StandardScaler:
-                The StandardScaler used to standardize the training data.
-            - dict:
-                The best hyperparameters found.
+            - matplotlib.figure.Figure:
+                The figure object for the confusion matrix.
+            - matplotlib.axes.Axes:
+                The axes object for the confusion matrix.
+            - float:
+                The accuracy of the SVM model.
+            - str:
+                The classification report of the SVM model.
     """
-    best_score = 0
-    best_params = {}
-    best_model = None
+    images_test_std = scaler.transform(images_test)
+    label_pred = model.predict(images_test_std)
+    label_score = model.decision_function(images_test_std)
 
-    # standardize the data
-    scaler = StandardScaler()
-    images_train_std = scaler.fit_transform(images_train)
-    images_val_std = scaler.transform(images_val)
+    # evaluate the model
+    fig, ax, accuracy, f1 = model_evaluation(labels_test, label_pred, label_score)
 
-    # set the hyperparamters
-    hyperparams = {
-        "C": [0.01, 0.1, 1, 10, 100],
-        "gamma": [0.01, 0.1, 1, 10, 100, "scale", "auto"] if kernal == "rbf" else [],
-    }
-
-    # serach for the best hyperparameters
-    for C1 in hyperparams["C"]:
-        for gamma1 in hyperparams["gamma"]:
-            # train the model
-            model = svm.SVC(kernel=kernal, C=C1, gamma=gamma1, random_state=711)
-            model.fit(images_train_std, labels_train)
-
-            # evaluate the model
-            label_pred = model.predict(images_val_std)
-            score = accuracy_score(labels_val, label_pred)
-
-            # update the best model
-            if score > best_score:
-                best_score = score
-                best_params["C"] = C1
-                best_params["gamma"] = gamma1
-                best_model = model
-
-    # print the best score
-    print(
-        f"With hyperparameter selection, the SVM's accuracy on the validation set is: {best_score}"
-    )
-
-    return best_model, scaler, best_params
+    return fig, ax, accuracy, f1
 
 
 def svm_test(
@@ -164,180 +245,133 @@ def svm_test(
     """
     images_std = scaler.transform(images)
     label_pred = model.predict(images_std)
+    label_score = model.predict_proba(images_std)
 
     # evaluate the model
-    fig, ax, accuracy, report = model_evaluation(labels, label_pred)
+    fig, ax, accuracy, f1 = model_evaluation(labels, label_pred, label_score)
 
-    return fig, ax, accuracy, report
-
-
-class cnn(nn.Module):
-    def __init__(self, num_classes: int):
-        super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 7, kernel_size=2, stride=1, padding=1),  # 1*28*28 -> 5*28*28
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 5*28*28 -> 5*14*14
-            nn.Conv2d(7, 11, kernel_size=2, stride=1, padding=1),  # 5*14*14 -> 11*14*14
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 11*14*14 -> 11*7*7
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(11 * 7 * 7, 128), nn.ReLU(), nn.Linear(128, num_classes)
-        )
-
-    def forward(self, x: torch.Tensor):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
+    return fig, ax, accuracy, f1
 
 
-# def cnn_params_init(model: nn.Module):
-#     """
-#     Initialize the CNN model parameters.
-
-#     Args:
-#         model (nn.Module): The CNN model to be initialized.
-#     """
-#     for m in model.modules():
-#         if isinstance(m, nn.Conv2d):
-#             nn.init.normal_(m.weight, mean=0, std=0.01)
-#             if m.bias is not None:
-#                 nn.init.constant_(m.bias, 0)
-#         elif isinstance(m, nn.Linear):
-#             nn.init.normal_(m.weight, mean=0, std=0.01)
-#             nn.init.constant_(m.bias, 0)
-
-
-def cnn_params_init(model: nn.Module):
-    """
-    Initialize the CNN model parameters with best practices.
-
-    Args:
-        model (nn.Module): The CNN model to be initialized.
-    """
-    for m in model.modules():
-        if isinstance(m, nn.Conv2d):
-            # He initialization
-            nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-            if m.bias is not None:
-                # initialize the bias to 0
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            # He initialization
-            nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-            if m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-
-
-def data_preprocess_for_cnn(
-    images: np.ndarray, labels: np.ndarray, batch_size: int, shuffle: bool
+def compute_kernel(
+    x1: np.ndarray, x2: np.ndarray, param_linear: float, param_rbf: float, gamma: float
 ):
     """
-    Preprocess the data for the CNN model.
+    Compute the kernel matrix.
 
     Args:
-        images (np.ndarray): Input images set.
-        labels (np.ndarray): Input labels set.
-        batch_size (int): The batch size for the data loader.
-        shuffle (bool): Whether to shuffle the data.
+        input (np.ndarray): Input data.
+        param_linear (float): The linear parameter.
+        param_rbf (float): The rbf parameter.
+        gamma (float): The gamma parameter.
 
     Returns:
-        torch.utils.data.DataLoader: The DataLoader object for the CNN model.
+        np.ndarray: The kernel matrix.
     """
-    # numpy -> tensor & normalize
-    images = torch.tensor(images, dtype=torch.float32).unsqueeze(1) / 255.0
-    labels = torch.tensor(labels, dtype=torch.long).squeeze(1)
+    if x2 is None:
+        x2 = x1
+    linear_kernel_matrix = linear_kernel(x1, x2)
+    rbf_kernel_matrix = rbf_kernel(x1, x2, gamma=gamma)
+    kernel_matrix = linear_kernel_matrix * param_linear + rbf_kernel_matrix * param_rbf
 
-    # print(labels.shape)
+    return kernel_matrix
 
-    # create dataset
-    dataset = TensorDataset(images, labels)
 
-    # load data
-    data_loader = DataLoader(
-        dataset, batch_size=batch_size, shuffle=shuffle, num_workers=0
+def multiple_kernel_svm_train(
+    images_train: np.ndarray,
+    labels_train: np.ndarray,
+    images_val: np.ndarray,
+    labels_val: np.ndarray,
+    param_linear: float,
+    param_rbf: float,
+    gamma: float,
+):
+    """
+    Train a multiple kernel SVM model.
+
+    Args:
+        images_train (np.ndarray): Input images training set.
+        labels_train (np.ndarray): Corresponding labels for the training set.
+        param_linear (float): The linear parameter.
+        param_rbf (float): The rbf parameter.
+        gamma (float): The gamma parameter.
+
+    Returns:
+        sklearn.svm.SVC: A trained Support Vector Classifier (SVC) object.
+    """
+    # standardize the data
+    scaler = StandardScaler()
+    images_train_std = scaler.fit_transform(images_train)
+
+    # compute the kernel matrix
+    kernel_matrix = compute_kernel(
+        images_train_std, None, param_linear, param_rbf, gamma
     )
 
-    return data_loader
-
-
-def cnn_train(
-    model: nn.Module,
-    train_loader: DataLoader,
-    val_loader: DataLoader,
-    criterion: nn.CrossEntropyLoss,
-    optimizer: optim.Adam,
-    num_epochs: int,
-):
-    train_loss = []
-    val_loss = []
-
-    # train set -> train the model
-    for epoch in range(num_epochs):
-        model.train()
-        running_loss = 0.0
-        for inputs_train, labels_train in tqdm(
-            train_loader, desc=f"epoch:{epoch+1} / {num_epochs}", unit="batch"
-        ):
-            optimizer.zero_grad()
-            outputs_train = model(inputs_train)
-
-            loss = criterion(outputs_train, labels_train)
-            loss.backward()
-
-            optimizer.step()
-            running_loss += loss.item() * inputs_train.size(0)
-
-        epoch_loss_train = running_loss / len(train_loader.dataset)
-        train_loss.append(epoch_loss_train)
-        print(f"epoch:{epoch+1} / {num_epochs}  Train Loss: {epoch_loss_train:.4f}")
-
-        # validation set -> adjust the hyperparameters
-        model.eval()
-        running_loss = 0.0
-        total = 0
-
-        with torch.no_grad():
-            for (
-                inputs,
-                labels,
-            ) in val_loader:
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
-                running_loss += loss.item() * inputs.size(0)
-
-        epoch_loss_val = running_loss / len(val_loader.dataset)
-        val_loss.append(epoch_loss_val)
-    fig, ax = train_val_loss_plot(train_loss, val_loss)
-
-    return fig, ax, model
-
-
-def cnn_test(model: nn.Module, test_loader: DataLoader, criterion: nn.CrossEntropyLoss):
-    model.eval()
-    running_loss = 0.0
-    all_preds = []
-    all_labels = []
-
-    with torch.no_grad():
-        for inputs, labels in test_loader:
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            running_loss += loss.item() * inputs.size(0)
-
-            # get the predictions
-            _, preds = torch.max(outputs, 1)
-            all_preds.append(preds)
-            all_labels.append(labels)
-
-    all_preds = torch.cat(all_preds).numpy()
-    all_labels = torch.cat(all_labels).numpy()
+    # train the model
+    mksvm_model = svm.SVC(
+        kernel="precomputed", C=1, class_weight="balanced", tol=0.001, random_state=711
+    )
+    mksvm_model.fit(kernel_matrix, labels_train)
 
     # evaluate the model
-    test_fig, test_ax, accuracy, report = model_evaluation(all_labels, all_preds)
-    test_loss = running_loss / len(test_loader.dataset)
+    images_val_std = scaler.transform(images_val)
+    kernel_matrix_val = compute_kernel(
+        images_val_std, images_train_std, param_linear, param_rbf, gamma
+    )
+    label_pred = mksvm_model.predict(kernel_matrix_val)
+    accuracy = accuracy_score(labels_val, label_pred)
 
-    return test_loss, test_fig, test_ax, accuracy, report
+    # print the accuracy
+    print(f"the Multiple Kernel SVM's accuracy on the validation set is: {accuracy}")
+
+    return mksvm_model, scaler
+
+
+def multiple_kernel_svm_test(
+    images_test: np.ndarray,
+    labels_test: np.ndarray,
+    image_train: np.ndarray,
+    model: svm.SVC,
+    param_linear: float,
+    param_rbf: float,
+    gamma: float,
+):
+    """
+    Test the multiple kernel SVM model.
+
+    Args:
+        images_test (np.ndarray): Input images test set.
+        labels_test (np.ndarray): Corresponding labels for the test set.
+        image_train (np.ndarray): Input images training set.
+        model (svm.SVC): The trained SVM model.
+        param_linear (float): The linear parameter.
+        param_rbf (float): The rbf parameter.
+        gamma (float): The gamma parameter.
+
+    Returns:
+        tuple:
+            - matplotlib.figure.Figure:
+                The figure object for the confusion matrix.
+            - matplotlib.axes.Axes:
+                The axes object for the confusion matrix.
+            - float:
+                The accuracy of the SVM model.
+            - str:
+                The classification report of the SVM model.
+    """
+    scaler = StandardScaler()
+    images_train_std = scaler.fit_transform(image_train)
+    images_test_std = scaler.transform(images_test)
+
+    # compute the kernel matrix
+    kernel_matrix_test = compute_kernel(
+        images_test_std, images_train_std, param_linear, param_rbf, gamma
+    )
+    label_pred = model.predict(kernel_matrix_test)
+    label_score = model.predict_proba(kernel_matrix_test)
+
+    # evaluate the model
+    fig, ax, accuracy, report = model_evaluation(labels_test, label_pred, label_score)
+
+    return fig, ax, accuracy, report
--- A/visualising.py
+++ A/visualising.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 from sklearn.metrics import (
     ConfusionMatrixDisplay,
     accuracy_score,
-    classification_report,
     confusion_matrix,
+    roc_auc_score,
 )
 
 
@@ -20,21 +22,47 @@ def train_val_loss_plot(train_loss: list, val_loss: list):
     return fig, ax
 
 
+def plot_label_distribution(labels: np.ndarray):
+
+    fig, ax = plt.subplots(figsize=(7, 6))
+
+    # flatten the label
+    labels = labels.flatten()
+    # count the number of each label
+    unique, counts = np.unique(labels, return_counts=True)
+
+    # plot the bar chart
+    ax.bar(unique, counts, tick_label=["malignant", "normal and benign"], width=0.5)
+    ax.set_xlabel("category")
+    ax.set_ylabel("sample number")
+    ax.set_title("Distribution of the training set")
+
+    return fig, ax
+
+
 # evaluate the model
-def model_evaluation(y_true, y_pred):
+def model_evaluation(y_true, y_pred, y_score):
     # plot the confusion matrix
     cm_matrix = confusion_matrix(y_true, y_pred)
-    cm_display = ConfusionMatrixDisplay(
-        confusion_matrix=cm_matrix, display_labels=["Benign", "Malignant"]
-    )
+    label_name = ["malignant", "normal and benign"]
     fig, ax = plt.subplots(figsize=(8, 8))
-    cm_display.plot(cmap="Blues", ax=ax)
-    plt.title("Confusion Matrix of BreastMNIST")
+    ax = sns.heatmap(
+        cm_matrix,
+        annot=True,
+        fmt="d",
+        cmap="Blues",
+        annot_kws={"size": 20},
+        xticklabels=label_name,
+        yticklabels=label_name,
+    )
+    ax.set_xlabel("Predicted label")
+    ax.set_ylabel("True label")
+    ax.set_title("Confusion Matrix")
 
     # calculate the accuracy
     accuracy = accuracy_score(y_true, y_pred)
 
-    # print the classification report
-    report = classification_report(y_true, y_pred, target_names=["Benign", "Malignant"])
+    # print the f1 score
+    auc = roc_auc_score(y_true, y_score)
 
-    return fig, ax, accuracy, report
+    return fig, ax, accuracy, auc
